bf514febd3346a9e30705105c769f01688b868c7
Soul <845948188@qq.com>
2019-06-10 18:07:17 +0800

    框架搭建

--- Hqb_App/src/main/java/com/bestnet/hf/HfHqbAppApplication.java
+++ Hqb_App/src/main/java/com/bestnet/hf/HfHqbAppApplication.java
@@ -2,8 +2,11 @@ package com.bestnet.hf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//Eureka客户端 消费者
+@EnableEurekaClient
 public class HfHqbAppApplication {
 
     public static void main(String[] args) {
