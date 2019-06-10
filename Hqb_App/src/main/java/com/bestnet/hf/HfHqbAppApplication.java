8163467e1880ad63c995836ccbd981654b047c70
Soul <845948188@qq.com>
2019-06-10 16:45:48 +0800

    Initial commit

---
+++ Hqb_App/src/main/java/com/bestnet/hf/HfHqbAppApplication.java
@@ -0,0 +1,13 @@
+package com.bestnet.hf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfHqbAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HfHqbAppApplication.class, args);
+    }
+
+}
---
+++ Hqb_Services/src/main/java/com/bestnet/hf/HfHqbServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bestnet.hf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfHqbServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HfHqbServicesApplication.class, args);
+    }
+
+}
